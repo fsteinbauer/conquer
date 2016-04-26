93ed82e5cb2ef6a2315be95ffc232ef949a66d8b
Trey <trevor.stammtisch@gmail.com>
2016-04-26 19:11:49 +0200

    [TR,MK] Added button animation functionality. Small changes to layout and functionality of MapsActivity

--- android/app/src/main/java/at/acid/conquer/MapsActivity.java
+++ android/app/src/main/java/at/acid/conquer/MapsActivity.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.location.Location;
-
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
+import at.acid.conquer.model.Route;
+import at.acid.conquer.model.TimeLocation;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,75 +26,66 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import at.acid.conquer.model.Route;
-import at.acid.conquer.model.TimeLocation;
-
 import static at.acid.conquer.LocationUtility.getSpeed;
 import static at.acid.conquer.LocationUtility.validDistance;
 
-public class MapsActivity extends FragmentActivity implements
-        OnMapReadyCallback,
-        LocationService.LocationServiceClient,
-        View.OnClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationService.LocationServiceClient, View.OnClickListener{
     public static final String TAG = "MapsActivity";
     public static final float DEFAULT_ZOOM = 16.0f;
 
-    private PolylineOptions mPathLineOptions = new PolylineOptions()
-            .width(10.0f)
-            .color(Color.RED);
+    private PolylineOptions mPathLineOptions = new PolylineOptions().width(10.0f).color(Color.RED);
 
     private GoogleMap mMap;
     private Route mRoute;
     private TimeLocation mLastLocation;
     private LocationService mLocationService;
 
-    private Button mButtonStart;
-    private Button mButtonStop;
+    private ImageButton mButtonStartStop;
+
 
     private TextView mTextArea;
     private TextView mTextInfo;
 
+    private boolean isTracking;
+
     // handle bidirection connection to LocationService
-    private ServiceConnection mLocationServiceConnection = new ServiceConnection() {
+    private ServiceConnection mLocationServiceConnection = new ServiceConnection(){
         @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
+        public void onServiceConnected(ComponentName className, IBinder service){
             mLocationService = ((LocationService.LocalBinder) service).getService();
             mLocationService.setServiceClient(MapsActivity.this);
         }
 
         @Override
-        public void onServiceDisconnected(ComponentName arg0) {
+        public void onServiceDisconnected(ComponentName arg0){
             mLocationService.setServiceClient(null);
             mLocationService = null;
         }
     };
 
     @Override//-------------------------------------------------------------------------------------
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         // Connect GUI elements
-        mButtonStart = (Button) findViewById(R.id.ButtonStart);
-        mButtonStop = (Button) findViewById(R.id.ButtonStop);
-        mButtonStart.setOnClickListener(this);
-        mButtonStop.setOnClickListener(this);
-        mButtonStop.setVisibility(View.GONE);
+        mButtonStartStop = (ImageButton) findViewById(R.id.ButtonPlayStop);
+        mButtonStartStop.setOnClickListener(this);
+
 
         mTextArea = (TextView) findViewById(R.id.TextArea);
         mTextInfo = (TextView) findViewById(R.id.TextInfo);
-        updateInfo(0,0);
+        updateInfo(0, 0);
         updateArea("Graz");
+
     }
 
     @Override//-------------------------------------------------------------------------------------
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap){
         Log.d(TAG, "onMapReady()");
         mMap = googleMap;
         mRoute = new Route(mMap, mPathLineOptions);
@@ -104,8 +95,7 @@ public class MapsActivity extends FragmentActivity implements
         location.setLatitude(47.067);
         location.setLongitude(15.433);
 
-        if (mLastLocation != null)
-            location = mLocationService.getLocation();
+        if(mLastLocation != null){ location = mLocationService.getLocation(); }
 
         mLastLocation = new TimeLocation(location);
         LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
@@ -113,18 +103,18 @@ public class MapsActivity extends FragmentActivity implements
     }
 
     @Override//-------------------------------------------------------------------------------------
-    public void onLocationUpdate(final Location location) {
-        if (location == null) {
+    public void onLocationUpdate(final Location location){
+        if(location == null){
             Log.d(TAG, "onLocationUpdate(): unknown position");
             return;
         }
 
-        if (mMap == null) {
+        if(mMap == null){
             Log.d(TAG, "onLocationUpdate(): wait for map");
 
             Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                public void run() {
+            timer.schedule(new TimerTask(){
+                public void run(){
                     onLocationUpdate(location);
                 }
             }, 1000);
@@ -137,12 +127,12 @@ public class MapsActivity extends FragmentActivity implements
         TimeLocation tloc = new TimeLocation(location);
 
         // distance to last valid point is valid - add point
-        if (validDistance(mRoute.getLastLocation(), tloc)) {
+        if(validDistance(mRoute.getLastLocation(), tloc)){
             mRoute.addLocationToCurrentPath(tloc);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(tloc.getLatLng()));
         }
         // distance to last point is valid - create new route
-        else if (validDistance(mLastLocation, tloc)) {
+        else if(validDistance(mLastLocation, tloc)){
             mRoute.addLocationToNewPath(mLastLocation, tloc);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(tloc.getLatLng()));
         }
@@ -150,9 +140,9 @@ public class MapsActivity extends FragmentActivity implements
         mLastLocation = tloc;
 
         List<TimeLocation> path = mRoute.getCurrentPath();
-        if( path != null && path.size() > 1 ) {
-            TimeLocation tloc1 = path.get(path.size()-1);
-            TimeLocation tloc2 = path.get(path.size()-2);
+        if(path != null && path.size() > 1){
+            TimeLocation tloc1 = path.get(path.size() - 1);
+            TimeLocation tloc2 = path.get(path.size() - 2);
             float kmh = getSpeed(tloc1, tloc2);
             float distance = mRoute.getDistance() / 1000; // distance in km
             updateInfo(kmh, distance);
@@ -172,18 +162,19 @@ public class MapsActivity extends FragmentActivity implements
     }
 
     @Override//-------------------------------------------------------------------------------------
-    public void onClick(View v) {
-        switch( v.getId() ){
-            case R.id.ButtonStart:{
-                mButtonStart.setVisibility(View.GONE);
-                mButtonStop.setVisibility(View.VISIBLE);
-                startTracking();
-                break;
-            }
-            case R.id.ButtonStop:{
-                mButtonStop.setVisibility(View.GONE);
-                mButtonStart.setVisibility(View.VISIBLE);
-                stopTracking();
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.ButtonPlayStop:{
+                if(isTracking){
+                    isTracking = false;
+                    stopTracking();
+                    TranslationAnimator.translateAndScaleCenter(mButtonStartStop);
+                } else{
+                    isTracking = true;
+                    TranslationAnimator.translateAndScaleToBottomRight(mButtonStartStop);
+                    startTracking();
+                }
+
                 break;
             }
         }
